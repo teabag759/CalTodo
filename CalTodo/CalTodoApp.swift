815c45051630237479c32af72c9862e4e7b759db
Lee Sohyeon <teabag759@gmail.com>
2024-04-15 15:36:19 +0900

    Initial Commit

---
+++ CalTodo/CalTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalTodoApp.swift
+//  CalTodo
+//
+//  Created by 이소현 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CalTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
